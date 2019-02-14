6964e55b7afaac658ac1b4b24f2d98553769f0ff
The Noah <thenoahbz@gmail.com>
2019-02-14 16:51:48 -0600

    Add InnerButton.java

---
+++ Dunbar/InnerButton.java
@@ -0,0 +1,39 @@
+package chap12;
+
+// Noah 2/13/2019
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class InnerButton{
+  JFrame frame;
+  JButton b;
+
+  public static void main(String[] args){
+    InnerButton gui = new InnerButton();
+    gui.go();
+  }
+
+  public void go(){
+    frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    b = new JButton("A");
+    b.addActionListener(new BListener());
+
+    frame.getContentPane().add(BorderLayout.SOUTH, b);
+    frame.setSize(200, 100);
+    frame.setVisible(true);
+  }
+
+  public class BListener implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+      if(b.getText().equals("A")){
+        b.setText("B");
+      }else{
+        b.setText("A");
+      }
+    }
+  }
+}
